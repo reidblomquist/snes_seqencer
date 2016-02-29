835aa8b2f8b6f1e5a2be6bd4cee529056657ba3e
Reid Blomquist <reid.blomquist@gmail.com>
2016-02-28 19:26:38 -0800

    clean up a bit; make hide delay relative to the BPMs instead of a hard int

--- Assets/Scripts/Audio Visualizer/HighlightOnPlay.cs
+++ Assets/Scripts/Audio Visualizer/HighlightOnPlay.cs
@@ -4,16 +4,20 @@ using System.Collections;
 public class HighlightOnPlay : MonoBehaviour {
 
 	public GameObject animateTarget;
+	public bool log;
 
 	private Sequencer seq;
 	private bool playingCurrent;
 	private int currentBeat;
-	private ParticleSystem parts;
+	private ParticleSystem parties;
+	private MeshRenderer meshy;
+	private int subSteps = 0;
 
-	void Awake ()
+	void Awake()
 	{
 		seq = transform.GetComponent<Sequencer>();
-		parts = animateTarget.GetComponent<ParticleSystem>();
+		parties = animateTarget.GetComponent<ParticleSystem>();
+		meshy = animateTarget.GetComponent<MeshRenderer>();
 	}
 	// Update is called once per frame
 	void Update()
@@ -26,16 +30,33 @@ public class HighlightOnPlay : MonoBehaviour {
 
 				if (playingCurrent)
 				{
-					animateTarget.GetComponent<MeshRenderer>().enabled = true;
-					parts.Play(true);
-					Debug.Log("OnBeat: " + seq._currentStep);
+					subSteps = 1;
+					meshy.enabled = true;
+					parties.Play(true);
+					if (log)
+					{
+						Debug.Log("OnBeat: " + seq._currentStep);
+						Debug.Log("subSteps: " + subSteps);
+					}
 				}
 				else
 				{
-					animateTarget.GetComponent<MeshRenderer>().enabled = false;
-					Debug.Log("OffBeat: " + seq._currentStep);
+					meshy.enabled = false;
+					if (log)
+					{
+						Debug.Log("OffBeat: " + seq._currentStep);
+						Debug.Log("subSteps: " + subSteps);
+					}
 				}
 			currentBeat = seq._currentStep;
+			}
+			else
+			{
+				subSteps++;
+			}
+			if (seq._currentStep == currentBeat && subSteps > seq.bpm * 0.04)
+			{
+				meshy.enabled = false;
 			}
 		}
 	}
