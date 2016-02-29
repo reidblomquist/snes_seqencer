1cf1ae236b4e4b1ed3b253d49cdf6b3c81178f50
Reid Blomquist <reid.blomquist@gmail.com>
2016-02-28 16:49:56 -0800

    bumpin to the base line

--- Assets/Scripts/Audio Visualizer/HighlightOnPlay.cs
+++ Assets/Scripts/Audio Visualizer/HighlightOnPlay.cs
@@ -3,30 +3,36 @@ using System.Collections;
 
 public class HighlightOnPlay : MonoBehaviour {
 
+	public GameObject animateTarget;
+
 	private Sequencer seq;
 	private bool playingCurrent;
 	private int currentBeat;
+	private ParticleSystem parts;
 
 	void Awake ()
 	{
 		seq = transform.GetComponent<Sequencer>();
-		Debug.Log(seq.sequence);
+		parts = animateTarget.GetComponent<ParticleSystem>();
 	}
 	// Update is called once per frame
-	void LateUpdate()
+	void Update()
 	{
 		if (seq.IsPlaying)
 		{
-			if (seq._currentStep != currentBeat || currentBeat == null)
+			if (seq._currentStep != currentBeat)
 			{
 				playingCurrent = seq.sequence[seq._currentStep - 1];
 
 				if (playingCurrent)
 				{
+					animateTarget.GetComponent<MeshRenderer>().enabled = true;
+					parts.Play(true);
 					Debug.Log("OnBeat: " + seq._currentStep);
 				}
 				else
 				{
+					animateTarget.GetComponent<MeshRenderer>().enabled = false;
 					Debug.Log("OffBeat: " + seq._currentStep);
 				}
 			currentBeat = seq._currentStep;
